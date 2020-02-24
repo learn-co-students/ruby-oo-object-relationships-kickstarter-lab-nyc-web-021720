3d1e5146df40be06da23ae517b8d0679d34f5e23
diana2341 <dianaponce2341@gmail.com>
2020-02-24 12:38:37 -0500

    Done.

---
+++ lib/project_backer.rb
@@ -0,0 +1,22 @@
+require "pry"
+class ProjectBacker
+    attr_reader :project, :backer
+    @@all = []
+
+    def initialize(project, backer)
+        @project = project
+        @backer = backer
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+end
+
+    
+ 
+# binding.pry
+"im the project_backer"
